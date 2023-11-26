67493245a369039b635ff5060ae8e54ce5669b19
Andy Bruce <abruce@uccs.edu>
2023-11-25 21:40:31 -0700

    Fixed issue: active carddecks not appearing on homepage. Need to fix description/author fields not appearing.

--- trivia_app/views.py
+++ trivia_app/views.py
@@ -24,10 +24,8 @@ def index(request):
 
 def index(request):
    #Need to fix the is_active issue.
-   #user_active_carddecks = User.objects.select_related('carddeck').all().filter(carddeck__is_active='True')
-   carddecks = User.objects.select_related('carddeck').all()
-   #print("active portfolio query set", user_active_carddecks)
-   return render( request, 'trivia_app/index.html', {'carddecks':carddecks})
+   active_carddecks = CardDeck.objects.all().filter(is_active='True')  
+   return render( request, 'trivia_app/index.html', {'carddeck_list':active_carddecks})
 
 
 class UserListView(generic.ListView):
